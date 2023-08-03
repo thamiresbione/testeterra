07c3476c9089512ccb99ebfcac4db9d8728fd385
Thamires Bione <thamiresbione@gmail.com>
2023-08-03 13:11:26 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
-  key_name = "krypton"
+  key_name = "krn"
 
   tags = {
     Name = "ExampleAppServerInstance"
